5b7493852d3ac123e243b12aa4e500c96689ef73
Михаил Буланкин <mvl.bulankin@gmail.com>
2022-11-22 20:34:17 +0300

    fix pep8

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -4,7 +4,7 @@ from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
-from posts.models import Follow, Group, Post, User
+from posts.models import Follow, Group, Post
 from .permissions import IsAuthorOrReadOnly
 from .serializers import (
     CommentSerializer,
